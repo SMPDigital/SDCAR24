41a25846183e6dcf47ddef4ea318159831c68aad
Nethika-Jayathilaka <nethika@smartannualreport.com>
2025-04-04 22:59:33 +0530

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -405,6 +405,21 @@ $(document).ready(function () {
   .from(".bod_main", {x: -20, autoAlpha: 0, duration: 0.5, stagger: 0.05})
 
 
+  let exetl = gsap.timeline({
+	scrollTrigger: {
+		trigger: ".exe_main_one",
+		start: "top 65%",
+		scrub: false,
+	  //   markers: true,
+		toggleActions: "play none none reverse"
+	}
+});
+
+
+exetl
+.from(".exe_main", {x: -20, autoAlpha: 0, duration: 0.5, stagger: 0.05})
+
+
   $(".NavSearchIcon").click(function () {
     $(".searchWrapper").addClass("active");
   });
